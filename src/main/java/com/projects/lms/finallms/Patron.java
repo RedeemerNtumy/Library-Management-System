2c545cfe27daee5914c5d7f2bba26440f14fefef
“Redeemer <ntumyredeemer@gmail.com>
2024-06-21 13:51:17 +0000

    Most recent working version

--- src/main/java/com/projects/lms/finallms/Patron.java
+++ src/main/java/com/projects/lms/finallms/Patron.java
@@ -1,28 +1,23 @@
 package com.projects.lms.finallms;
 
-public class Patron {
+public class Patron extends User {
     private int patronID;  // Auto-incremented by the database
-    private String name;
-    private String email;
 
     public Patron(String name, String email) {
-        this.name = name;
-        this.email = email;
+        super(name, email);
     }
-    // Getters and Setters
+
+
     public int getPatronID() {
         return patronID;
     }
+
     public void setPatronID(int patronID) {
         this.patronID = patronID;
     }
-    public String getName() {return name;}
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getEmail() {
-        return email;
-    }
+
     @Override
-    public String toString() {return name;}  // Return the patron's name for display in the ComboBox
+    public String toString() {
+        return getName();  // Return the patron's name for display in the ComboBox
+    }
 }
