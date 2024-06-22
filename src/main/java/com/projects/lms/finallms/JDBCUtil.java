e870857dace00931164aae3603b23f8a5ea94f7c
“Redeemer <ntumyredeemer@gmail.com>
2024-06-22 17:34:38 +0000

    Final update

--- src/main/java/com/projects/lms/finallms/JDBCUtil.java
+++ src/main/java/com/projects/lms/finallms/JDBCUtil.java
@@ -6,10 +6,13 @@ import java.sql.SQLException;
 
 public class JDBCUtil {
     private static final String URL = "jdbc:mysql://127.0.0.1:3306/LibraryDB";
-    private static final String USER = "root";
-    private static final String PASSWORD = "redeemerntumy4019";
+    private static final String USER = "username";
+    private static final String PASSWORD = "password";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
+
+
+
